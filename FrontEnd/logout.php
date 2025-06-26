723d2c3545169c218c113c12b19756d863285b08
Isabela de França Lima <20232ewbj0122@belojardim.ifpe>
2025-06-25 21:14:31 -0300

    Corrige caminhos dos links no logout e área do professor

--- FrontEnd/logout.php
+++ FrontEnd/logout.php
@@ -4,6 +4,6 @@ session_unset(); // Limpa as variáveis da sessão
 session_destroy(); // Destroi a sessão
 
 // Redireciona para a página de login ou inicial
-header("Location: ../index.html");
+header("Location: ../FrontEnd/index.html");
 exit;
 ?>
